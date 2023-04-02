292a7272da373d35193ccedb949f4e9374438c52
Anup Jung Thapa <np03cs4a220496@heraldcollege.edu.np>
2023-04-02 21:30:50 +0545

    Added some others workshop files

--- week4/arrayManipulation.js
+++ week4/arrayManipulation.js
@@ -1,17 +1,15 @@
+let num=[1,2,3,4,5];
 function arrayManipulation(num, callback1, callback2){
     const operation=num.map(callback1);
     const filter=operation.filter(callback2);
     return filter;
 }
-let num=[1,2,3,4,5];
+
 function callback1(num){
     return num*num;
 }
 function callback2(num){
     return num<16;
 }
-
-// document.getElementById('para').innerHTML=arrayManipulation(num,callback1,callback2);
 const result=arrayManipulation(num,callback1,callback2);
-// console.log(result);
 document.getElementById('para').innerHTML=result;
